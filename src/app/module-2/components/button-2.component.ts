cafd5def477faec298c7b117dd66d75ceed66e2b
jjmato <jjmatotorres@gmail.com>
2020-05-26 14:15:29 +0200

    2 files modified

--- src/app/module-2/components/button-2.component.ts
+++ src/app/module-2/components/button-2.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button-2',
-  template: `<h1>Hello {{name}}!</h1>`,
+  templateUrl: './button-2.component.html',
   styles: [`h1 { font-family: Lato; }`]
 })
 export class Button2Component  {
